26bf7e86fb7e62b0cb1773434634a4cfb46f9f2b
526LGreen@frhsd.com <526LGreen@frhsd.com>
2024-11-13 09:30:19 -0500

    Pt 4 of working on my story

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let firstAdjective = prompt('Give me an adjective');
 let secondAdjective = prompt('Give me another adjective');
 
 
-let myStory =`${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${firstNoun} but as much as they tried and train they just couldn't. Everyone called ${characterName} a ${firstAdjective} and it really hurt their feelings. But ${characterName} was going to prove them ${secondAdjective}` 
+
+
+let myStory =`${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${firstNoun} but as much as they tried and train they just couldn't. Everyone called ${characterName} a ${firstAdjective} and it really hurt their feelings. But ${characterName} was going to prove them ${secondAdjective}, "just you wait" ${characterName} said.` 
 console.log(myStory) 
 
 document.getElementById('story').innerHTML = myStory;

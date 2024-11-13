1cf9e3d34187ca22463478f0f8e369789dddb9da
526LGreen@frhsd.com <526LGreen@frhsd.com>
2024-11-13 09:23:09 -0500

    Pt 3 of working on my story

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let characterName = prompt('What do you want the name of the main character to be?');
-let firstNoun = prompt('Give me a noun');
+let firstNoun = prompt('Give me a plural noun');
 let firstAdjective = prompt('Give me an adjective');
-console.log(firstNoun)
+let secondAdjective = prompt('Give me another adjective');
 
 
-let myStory =`${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${firstNoun} but as much as they tried and train they just couldn't.` 
+let myStory =`${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${firstNoun} but as much as they tried and train they just couldn't. Everyone called ${characterName} a ${firstAdjective} and it really hurt their feelings. But ${characterName} was going to prove them ${secondAdjective}` 
 console.log(myStory) 
 
 document.getElementById('story').innerHTML = myStory;

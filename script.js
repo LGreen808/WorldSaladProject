5fd23b80fcba790f3ed22b24646f9bf3402297ee
LGreen808 <526LGreen@frhsd.com>
2024-11-29 23:33:42 +0000

    Pt 5 of working on my story

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let characterName = prompt('What do you want the name of the main character to be?');
-let firstNoun = prompt('Give me a plural noun');
+const characterName = prompt('What do you want the name of the main character to be?');
+let pluralNoun = prompt('Give me a plural noun');
 let firstAdjective = prompt('Give me an adjective');
 let secondAdjective = prompt('Give me another adjective');
+let secondNoun = prompt('Give me another noun');
+const thirdNoun = prompt('Give me a magical pet');
+let firstVerb = prompt('Give me a verb');
+const antagonistName = prompt('What do you want the name of the antagonist to be?');
+let thirdAdjective = prompt('Give me a mean adjective ex:(jerk, meanie, curse word)')
+const challengeStatement = prompt('Write a statement for the main character to challenge the antagonist to a race');
+let placeRace = prompt('Where should the race be?');
+
+let myStory =`${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${pluralNoun} but as much as they tried and trained they just couldn't. 
+Everyone called ${characterName} a ${firstAdjective} and it really hurt their feelings. But ${characterName} was going to prove them ${secondAdjective}, "just you wait" ${characterName} said. 
+Then one day a magic ${secondNoun} appeared out of nowhere and told ${characterName} that they could make ${characterName} be able to run fast but ${characterName} had to give ${secondNoun} there ${thirdNoun} and then ${characterName} would be able to run super duper fast.
+The next day ${characterName} ${firstVerb} to ${antagonistName} who was a real big ${thirdAdjective} and said, ${challengeStatement}. 
 
 
 
 
-let myStory =`${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${firstNoun} but as much as they tried and train they just couldn't. Everyone called ${characterName} a ${firstAdjective} and it really hurt their feelings. But ${characterName} was going to prove them ${secondAdjective}, "just you wait" ${characterName} said.` 
 console.log(myStory) 
 
 document.getElementById('story').innerHTML = myStory;

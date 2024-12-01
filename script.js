7c886633af808c096093c64a787e2b0a7f4ea215
LGreen808 <526LGreen@frhsd.com>
2024-12-01 23:50:28 +0000

    Finished my story

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const characterName = prompt('What do you want the name of the main character to
 let pluralNoun = prompt('Give me a plural noun');
 let firstAdjective = prompt('Give me an adjective');
 let secondAdjective = prompt('Give me another adjective');
-let secondNoun = prompt('Give me another noun');
+const secondNoun = prompt('What is your favorite wild animal?');
 const thirdNoun = prompt('Give me a magical pet');
 let firstVerb = prompt('Give me a verb in the past tense');
 const antagonistName = prompt('What do you want the name of the antagonist to be?');
 let thirdAdjective = prompt('Give me a mean adjective ex:(jerk, meanie, curse word)')
 const challengeStatement = prompt('Write a statement for the main character to challenge the antagonist to a race');
 let placeRace = prompt('Where should the race be?');
-const fourthNoun = prompt('Give me a magical ANIMAL this time please');
-let secondVerb = prompt('Give me a verb please');
-const randomLine = prompt('Give me a random line please, seriously, just anything, anything that comes into your head');
+const fourthNoun = prompt('What is your favorite magical animal? (ex: Unicorn, dragon, phoenix, etc)');
+let secondVerb = prompt('Give me another verb in the past tense please');
+const randomLine = prompt('Give me a random line you can say to someone, please, seriously, just anything, anything that comes into your head');
+let fourthAdjective = prompt('Winning or loosing....trust me this makes sense');
+const fifthNoun = prompt('One or the other...cat or dog?');
+
 
 let myStory = `${characterName} never ran really fast. ${characterName} just wanted to run really fast like their ${pluralNoun} but as much as they tried and trained they just couldn't. 
 Everyone called ${characterName} ${firstAdjective} and it really hurt their feelings. But ${characterName} was going to prove them ${secondAdjective}, "just you wait" ${characterName} said. 
 Then one day a magic ${secondNoun} appeared out of nowhere and told ${characterName} that they could make them ${characterName} be able to run fast but ${characterName} had to give ${secondNoun} there ${thirdNoun} 
 and then ${characterName} would be able to run super duper fast. The next day ${characterName} ${firstVerb} to ${antagonistName} who was a real big ${thirdAdjective} and said, "${challengeStatement}". 
-At the race ${characterName} was very nervous but then the magical ${fourthNoun} appeared, ${secondVerb} and told ${characterName} that they were going to `
+At the race ${characterName} was very nervous but then the magical ${fourthNoun} appeared, ${secondVerb} to ${characterName} and told ${characterName} ${randomLine}. Then boom! the race started and ${characterName} was ${fourthAdjective}.
+But sadly ${characterName} lost, turns out ${secondNoun} was just a ${fifthNoun}. The end!.`
 console.log(myStory) 
 
 document.getElementById('story').innerHTML = myStory;
